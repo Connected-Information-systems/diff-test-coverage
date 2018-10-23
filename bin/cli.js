4376c0b42f79518b562e4e2ecfb06e38d34f739b
Mark Lagendijk <mark@lagendijk.nl>
2018-10-23 13:26:19 +0200

    Nice logging of the results. Added flags for base dirs and thresholds. Percentage colors using chalk, based on thresholds. Exit code of 1 when thresholds are not met.

--- bin/cli.js
+++ bin/cli.js
@@ -19,7 +19,34 @@ const argv = require('yargs')
             describe: 'The type of coverage report. When provided multiple times the types will be mapped to the matching coverage glob patterns.',
             type: 'array',
             choices: coverageParser.types
-        }
+        },
+        l: {
+            alias: 'line-coverage',
+            describe: 'Required line coverage percentage on the diff. The application will exit with -1 if this is not reached.',
+            type: 'number',
+            default: 80
+        },
+        b: {
+            alias: 'branch-coverage',
+            describe: 'Required branch coverage percentage on the diff. The application will exit with -1 if this is not reached.',
+            type: 'number',
+            default: 80
+        },
+        'coverage-base-dir': {
+            describe: 'The base directory for resolving relative paths in coverage reports. Uses current working directory by default.',
+            type: 'string',
+            default: process.cwd()
+        },
+        'diff-base-dir': {
+            describe: 'The base directory for resolving relative paths in the diff. Uses current working directory by default.',
+            type: 'string',
+            default: process.cwd()
+        },
+        'diff-coverage-base-dir': {
+            describe: 'The base directory for displaying the diff coverage results. Uses current working directory by default.',
+            type: 'string',
+            default: process.cwd()
+        },
     })
     .demand(1)
     .example(`hg diff -r 'p1(min(branch(CPB-1352))):CPB-1352' | diff-test-coverage -c **/lcov.info -t lcov --`)
@@ -29,11 +56,22 @@ const argv = require('yargs')
 application.run({
     coverageReports: {
         globs: argv.coverage,
-        types: argv.type
+        types: argv.type,
+        baseDir: argv.coverageBaseDir
     },
     diff: {
         text: argv._[0],
-        baseDir: process.cwd()
+        baseDir: argv.diffBaseDir
     }
-}).then(filteredCoverageByFile => coverageLogger.log(filteredCoverageByFile, process.cwd()));
-
+}).then(({ coverageByFile, totals }) => {
+    coverageLogger.log(coverageByFile, totals, {
+        baseDir: argv.diffCoverageBaseDir,
+        coverageThresholds: {
+            lines: argv.lineCoverage,
+            branches: argv.branchCoverage
+        }
+    });
+    if(totals.lines.percentage < argv.lineCoverage || totals.branches.percentage < argv.branchCoverage){
+        process.exitCode = 1;
+    }
+});
